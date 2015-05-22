67c0796f511e2f98840d4518691f723515566e2e
Prashant Chandrasekar <peecee.prashant@gmail.com>
2015-05-21 22:20:49 -0400

    Changes path of home directory

--- sitestoryserversetup.sh
+++ sitestoryserversetup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ARCHIVE_DIR=ephemeral
 sudo yum -y install httpd httpd-devel gcc git curl-devel apr-devel openssl-devel java
-sudo mkdir ~/development
+sudo mkdir /home/zhiwuxie/development
 cd development
 sudo git clone https://github.com/peeceeprashant/UWS.git
 sudo git clone https://github.com/peeceeprashant/SiteStory.git
@@ -10,10 +10,10 @@ sudo cp apache-tomcat-6.0.41.tar.gz ../
 cd ..
 sudo tar -xvf apache-tomcat-6.0.41.tar.gz
 cd apache-tomcat-6.0.41/webapps/
-sudo cp ~/development/SiteStory/sitestory-webapp/target/sitestory.war .
+sudo cp /home/zhiwuxie/development/SiteStory/sitestory-webapp/target/sitestory.war .
 cd ..
 sudo bin/startup.sh
 #Change directory where archives will be stored
-sudo sed -i "s/backup/$ARCHIVE_DIR/g" ~/development/apache-tomcat-6.0.41/webapps/sitestory/WEB-INF/classes/ta.properties
+sudo sed -i "s/backup/$ARCHIVE_DIR/g" /home/zhiwuxie/development/apache-tomcat-6.0.41/webapps/sitestory/WEB-INF/classes/ta.properties
 sudo bin/shutdown.sh
 sudo bin/startup.sh
